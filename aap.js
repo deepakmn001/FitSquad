0967a785551c00072f7fbaef284d0d4e19dfaafe
deepakmn001 <adeepakkumar975@gmail.com>
2025-04-07 22:59:56 +0530

    Update aap.js

--- aap.js
+++ aap.js
@@ -62,3 +62,27 @@ auth.onAuthStateChanged((user) => {
     window.location.href = "login.html";
   }
 });
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBq30WASNqZkOpz3cCR9_IeR1y9PHwGbrM",
+    authDomain: "fitsquad-bce50.firebaseapp.com",
+    databaseURL: "https://fitsquad-bce50-default-rtdb.firebaseio.com",
+    projectId: "fitsquad-bce50",
+    storageBucket: "fitsquad-bce50.firebasestorage.app",
+    messagingSenderId: "602575671919",
+    appId: "1:602575671919:web:cf79a9e165321041e4b39a",
+    measurementId: "G-70M11S5CZC"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
